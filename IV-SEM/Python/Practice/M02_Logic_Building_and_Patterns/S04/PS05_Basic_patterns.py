7b75db3a734f2cd1d7b70668b01a6ae48acbb2db
Nikhilreddy139 <muthyalanikhilreddy1@gmail.com>
2026-03-06 09:49:36 +0530

    my first commit 6032026

--- IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S04/PS05_Basic_patterns.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S04/PS05_Basic_patterns.py
@@ -15,3 +15,9 @@ for i in range(n,0,-1):
         print("*",end=' ')
     print()
 '''
+n = int(input())
+for i in range(n+1):
+    for j in range(i):
+        print(i+j,end=' ')
+        i = i+1
+    print()
